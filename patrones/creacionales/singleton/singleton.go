8d3e7aee56287ca05c23b24f77ba9f7875309a9a
Daniel M. Spiridione <dspiridione@ewwoconsulting.com>
2018-11-07 11:07:23 -0300

    Edicion singleton

--- patrones/creacionales/singleton/singleton.go
+++ patrones/creacionales/singleton/singleton.go
@@ -12,35 +12,35 @@ type Singleton struct {
 }
 
 // Creador "estático"
-var instance *Singleton
+var instancia *Singleton
 var once sync.Once
 
-func GetInstance() *Singleton {
+func GetInstancia() *Singleton {
 	once.Do(func() {
-		instance = &Singleton{
+		instancia = &Singleton{
 			time.Now().Unix(),
 		}
 	})
 
-	return instance
+	return instancia
 }
 
 // Test de uso del patrón
 func TestPattern() {
 	fmt.Println("Todas las instancias Singleton tienen que tener el mismo número")
 
-	fmt.Printf("Instancia Singleton: %d\n", GetInstance().Tiempo)
+	fmt.Printf("Instancia Singleton: %d\n", GetInstancia().Tiempo)
 
 	time.Sleep(1 * time.Second)
 
-	fmt.Printf("Instancia Singleton: %d\n", GetInstance().Tiempo)
+	fmt.Printf("Instancia Singleton: %d\n", GetInstancia().Tiempo)
 
 	canalEspera := make(chan int64)
 
 	go func() {
 		time.Sleep(1 * time.Second)
 
-		canalEspera <- GetInstance().Tiempo
+		canalEspera <- GetInstancia().Tiempo
 	}()
 
 	fmt.Printf("Instancia Singleton: %d\n", <-canalEspera)
